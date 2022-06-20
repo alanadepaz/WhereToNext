b69b483b13459755320a9704ecf8a6cbae61264f
Alana Depaz <alanadepaz@fb.com>
2022-06-20 14:31:26 -0700

    Completed simple sign up activity.

--- app/src/main/java/com/example/wheretonext/MainActivity.java
+++ app/src/main/java/com/example/wheretonext/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.wheretonext;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logoutButton) {
+            // Compose icon has been selected
+            ParseUser.logOutInBackground();
+
+            // Navigate to the compose activity
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
