0fdc064706eda62b670d9dc184670f12772299b6
Alana Depaz <alanadepaz@fb.com>
2022-07-21 11:20:40 -0700

    Added functionality where if a user clicks a country, it will be highlighted

--- app/src/main/java/com/alana/wheretonext/ui/map/MapFragment.java
+++ app/src/main/java/com/alana/wheretonext/ui/map/MapFragment.java
@@ -1,5 +1,6 @@
 package com.alana.wheretonext.ui.map;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.location.Address;
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -22,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alana.wheretonext.R;
+import com.alana.wheretonext.service.Reversegeo.ReverseGeoCoder;
 import com.alana.wheretonext.ui.phrases.PhrasesActivity;
 import com.alana.wheretonext.utils.CountryUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -33,14 +36,20 @@ import com.google.maps.android.data.Feature;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
 import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import kotlin.TypeCastException;
+import kotlin.jvm.internal.Intrinsics;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,6 +69,69 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     // Widgets
     private AutoCompleteTextView mSearchText;
 
+    // For highlighting the clicked countries
+    @NotNull
+    private Map layers = (Map)(new LinkedHashMap());
+    public ReverseGeoCoder reverseGeoCode;
+    @Nullable
+    private String country = "";
+    @Nullable
+    private String prevCountry = "";
+    @Nullable
+    private InputStream inputStream;
+
+    @NotNull
+    public final Map getLayers() {
+        return this.layers;
+    }
+
+    public final void setLayers(@NotNull Map map) {
+        Intrinsics.checkParameterIsNotNull(map, "<set-?>");
+        this.layers = map;
+    }
+
+    @NotNull
+    public final ReverseGeoCoder getReverseGeoCode() {
+        ReverseGeoCoder reverseGeoCoder = this.reverseGeoCode;
+        if (reverseGeoCoder == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("reverseGeoCode");
+        }
+
+        return reverseGeoCoder;
+    }
+
+    public final void setReverseGeoCode(@NotNull ReverseGeoCoder reverseGeoCode) {
+        Intrinsics.checkParameterIsNotNull(reverseGeoCode, "<set-?>");
+        this.reverseGeoCode = reverseGeoCode;
+    }
+
+    @Nullable
+    public final String getCountry() {
+        return this.country;
+    }
+
+    public final void setCountry(@Nullable String country) {
+        this.country = country;
+    }
+
+    @Nullable
+    public final String getPrevcountry() {
+        return this.prevCountry;
+    }
+
+    public final void setPrevcountry(@Nullable String prevCountry) {
+        this.prevCountry = prevCountry;
+    }
+
+    @Nullable
+    public final InputStream getInputStream() {
+        return this.inputStream;
+    }
+
+    public final void setR(@Nullable InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
     public MapFragment() {
         // Required empty public constructor
     }
@@ -68,12 +140,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MapFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
+    public static MapFragment newInstance() {
         return new MapFragment();
     }
 
@@ -97,7 +166,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
-        mapFragment.getMapAsync(this);
+        if (mapFragment == null) {
+            throw new TypeCastException("null cannot be cast to non-null type com.google.android.gms.maps.SupportMapFragment");
+        } else {
+            mapFragment.getMapAsync((OnMapReadyCallback)this);
+            this.inputStream = this.getResources().openRawResource(R.raw.cities15000);
+            try {
+                this.reverseGeoCode = new ReverseGeoCoder(this.inputStream, false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         mSearchText = (AutoCompleteTextView) view.findViewById(R.id.input_search);
 
@@ -209,9 +288,65 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         init(googleMap);
         Log.i(TAG, "Google map ready.");
 
+        String[] isoCountryCodes = Locale.getISOCountries();
+
+        //Iterate through all countries and preprocess GeoJsonLayer to be added onclick
+        for (String isoCountryCode : isoCountryCodes) {
+            String iso2code = isoCountryCode.toLowerCase();
+
+            // Handle corner cases
+            if (iso2code == "do") {
+                iso2code = "doo";
+            }
+            // Remove Israel
+            if (iso2code == "il") {
+                iso2code = "ps";
+            }
+
+            // Check if the GeoJson file exists
+            int fileExists = this.getResources().getIdentifier(iso2code, "raw", "com.alana.wheretonext");
+
+            // If the file exists, preprocess the Geojson layer
+            GeoJsonLayer layer = null;
+            if (fileExists != 0) {
+                try {
+                    layer = new GeoJsonLayer(googleMap, this.getResources().getIdentifier(iso2code, "raw", "com.alana.wheretonext"), getContext());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                GeoJsonPolygonStyle style = layer.getDefaultPolygonStyle();
+                style.setFillColor(Color.CYAN);
+                style.setStrokeColor(Color.CYAN);
+                style.setStrokeWidth(1F);
+
+                layers.put(iso2code, layer);
+            }
+
+        }
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng position) {
+                country = getReverseGeoCode().nearestPlace(position.latitude, position.longitude).country.toLowerCase();
+
+                if (country == null) {
+                    throw new NullPointerException("null cannot be cast to non-null type java.lang.String");
+                } else {
+                    GeoJsonLayer newLayer = (GeoJsonLayer) getLayers().get(getCountry());
+                    if (newLayer != null) {
+                        newLayer.addLayerToMap();
+                    }
+
+                    newLayer = (GeoJsonLayer) getLayers().get(getPrevcountry());
+                    if (newLayer != null) {
+                        newLayer.removeLayerFromMap();
+                    }
+
+                    setPrevcountry(getCountry());
+                }
+
                 //Get the lat long here from variable position
                 //Use GeoCoder class to fetch the country from latlong like this
                 Geocoder geocoder = new Geocoder(getContext());
@@ -225,7 +360,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     String country = addresses.get(0).getCountryName();
                     Log.i(TAG, "Country name: " + country);
 
-                    goPhrasesActivity(country);
+                    // Delay navigation to next activity just enough to see highlighted country
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable(){
+                        @Override
+                        public void run(){
+                            goPhrasesActivity(country);
+                        }
+                    }, 200);
                 }
             }
         });
